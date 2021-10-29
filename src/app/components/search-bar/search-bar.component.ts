322d727032d24f2a6171b5eebdb98e5eadd79a61
John Mazuera <jdm42762@huskies.bloomu.edu>
2021-10-29 01:26:45 -0400

    Messed up the last commit needed to be redone

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-=======
->>>>>>> 5df23eff489521302792f4984b49410d984fc9ad
 
 @Component({
   selector: 'app-search-bar',
@@ -12,20 +9,13 @@ import { Router } from '@angular/router';
 })
 export class SearchBarComponent implements OnInit {
 
-<<<<<<< HEAD
   constructor(private router: Router) { }
-=======
-  constructor() { }
->>>>>>> 5df23eff489521302792f4984b49410d984fc9ad
 
   ngOnInit(): void {
   }
 
-<<<<<<< HEAD
   onSubmit(form: NgForm) {
     this.router.navigate(['search', form.value.search]);
   }
 
-=======
->>>>>>> 5df23eff489521302792f4984b49410d984fc9ad
 }
